42192080e5501da34d84a81517ce87be7626342b
Dan Newcomb <dan111174@gmail.com>
2024-05-03 15:27:07 -0500

    push new dashboard

--- PekinADA/cdn/1/widgets/layout/fixed/dist/runtime/translations/hr.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Fiksna ploča",widgetProperties:"Vlasništva widgeta",widgetFunctions:"Funkcije widgeta",widgetName:"naziv widgeta:",widgetProps:"vlasništva widgeta:",tips:"Fiksna ploča"})}}}));
--- PekinADA/cdn/1/widgets/layout/fixed/dist/runtime/translations/sk.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Pevný panel",widgetProperties:"Vlastnosti widgetu",widgetFunctions:"Funkcie widgetu",widgetName:"názov widgetu:",widgetProps:"vlastnosti widgetu:",tips:"Pevný panel"})}}}));
