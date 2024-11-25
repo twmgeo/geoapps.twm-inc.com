e05a42cd45eee30715e235b03bf48ff29e41668f
Dan Newcomb <dan111174@gmail.com>
2024-11-25 11:49:04 -0600

    fix logo

---
+++ OFallonIL/Pavement/cdn/11/widgets/common/chart/dist/runtime/translations/default.ts
@@ -0,0 +1,9 @@
+export default {
+  _widgetLabel: 'Chart',
+  bar1SeriesLimitation: 'Bar or column charts with one series are limited to 10,000 bars in total. Please filter or re-aggregate your data and try again.',
+  bar2SeriesLimitation: 'Bar or column charts with two series are limited to 2,000 bars, or 1,000 bars per series. Please filter or re-aggregate your data and try again.',
+  bar3SeriesLimitation: 'Bar or column charts with three or more series are limited to 2,000 bars, or 100 bars per series. Please filter or re-aggregate your data and try again.',
+  lineLimitation: 'Line or area charts are limited to 10,000 value markers in total and maximum of 100 series. Please filter or re-aggregate your data and try again.',
+  pieLimitation: 'Cannot render, too many categories. Pie chart is limited to 300 categories as the maximum.',
+  dataEmptyTip: 'No data found.'
+}
